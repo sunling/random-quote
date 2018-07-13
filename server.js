296b5df0370b54481bd2be7c5a53a5dceb25182d
Ling Sun <lingsun@mum.edu>
2018-07-13 18:09:03 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 const express = require('express');
-const http = require('http')
-const path = require('path');
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
- 
-const port = process.env.PORT || 8080;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log('running'));
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
